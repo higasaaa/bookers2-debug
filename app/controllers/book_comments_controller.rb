c4a2130aaf0b8dd92ea85e08eef59ff5353eacfc
higasaaa <higasaaa26@gmail.com>
2023-01-09 12:50:22 +0000

    非同期通信化[Finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referer #特定の処理後、遷移元のURLに戻る
+    @book_comment = BookComment.new #@book_comment = BookComment.newとしてるのは、投稿が残らないようにする
+    # redirect_to request.referer #特定の処理後、遷移元のURLに戻る
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy #find_by条件を指定して最初の1件を取得存在しない場合はnil
-    redirect_to request.referer
+    @book = Book.find(params[:book_id]) #追加
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]) #find_by条件を指定して最初の1件を取得存在しない場合はnil
+    @book_comment.destroy
+    # @book_comment = BookComment.new
+    # redirect_to request.referer
   end
 
   private
